76b06c084afbe6326399e85f634a9f96ec9a3721
Barış Bora <baris.bora@itu.edu.tr>
2018-07-14 15:19:27 +0300

    update

---
+++ src/QueryBuilderServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Barisbora\QueryBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class QueryBuilderServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+}
